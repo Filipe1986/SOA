fe4f14082a519f9ac2a918f339104b1a33d10854
Filipe <filipe.goncalves@uniriotec.br>
2017-06-05 20:10:38 -0300

    Todos os projetos

--- ConfigPostgresql.py
+++ ConfigPostgresql.py
@@ -3,10 +3,11 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET'])
 def get_publicacao():
     con = psycopg2.connect(host='localhost', database='publicacoes',
-                       user='postgres', password='admin')
+                           user='postgres', password='admin')
     cur = con.cursor()
     con.commit()
     cur.execute('SELECT * FROM publicacao')
